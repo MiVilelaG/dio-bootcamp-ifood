333dfeace77358b3b3d62043bedc15dc8f259d5a
MiVilelaG <105995942+MiVilelaG@users.noreply.github.com>
2023-04-26 16:02:59 -0300

    ok

---
+++ JS/tempo-estimado-entrega.js
@@ -0,0 +1,7 @@
+//faça um programa que fale o nome do restaurante e o tempo estimado da entrega.
+const nomeRestaurante = MCDonalds;
+const tempoEstimadoEntrega = 10;
+
+let mensagem = `O restaurante ${nomeRestaurante} entrega em ${tempoEstimadoEntrega} minutos.`;
+
+console.log(mensagem);
